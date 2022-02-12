76fae359a4f2de8d4931849e4f48cd188df10994
Oskar Debski <oskardebski1994@gmail.com>
2022-02-12 18:53:04 +0000

    Add index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+function playRound(playerSelection, computerSelection) {
+    
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
